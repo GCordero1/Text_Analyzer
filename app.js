d04dd40bde6af123c9f93f13e86c68ef8be250ec
GCordero1 <Gerson.Cordero@gmail.ccom>
2017-07-31 21:34:58 -0400

    added comments

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 function getAverageWordLength(letters) {
+	// join letters together creating a string
+	// divide the total length by the number of letters
+	// to get average
   var totalLength = letters.join("").length;
   return (totalLength / letters.length).toFixed(2);
 }
 
-function countDistinctWords(letters)
+function countDistinctWords(letters) {
+	// empty array of distinct words.
 	var distinctWords = [];
+	// iterations occur checking for repetition and distinct words are added to list.
 	for (var i=1; i < letters.length; i++) {
 		if (distinctWords.indexof(letters[i]) === -1) {
 			distinctWords.push(letters[i]);
 		}
 	}
 
-	return distinctWords.letters;
+	return distinctWords.length;
 }
 
-function standardizeText(text)
+// standardizing text by removing punctuation, changing all text to lowerCase
+// therefore making inputs consistent.
+function standardizeText(text) {
 	return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
 }
 
 function removeReturns(text) {
   return text.replace(/\r?\n|\r/g, "");
+}
 
 function reportOnText(text) {
 	var letters = standardizeText(text);
@@ -27,6 +35,7 @@ function reportOnText(text) {
 	var numTotalWords = letters.length;
 	var averageWordLength = getAverageWordLength(letters);
 
+// diplay data on DOM.
 	var textReport = $('.js-text-report');
 	textReport.find('.js-word-count').text(numTotalWords);
 	textReport.find('.js-unique-word-count').text(numDistinctWords);
@@ -35,9 +44,11 @@ function reportOnText(text) {
 	textReport.removeClass('hidden');
 }
 
+// watch for and handle form submissions
 function watchFormSubmission() {
 	$('.js-text-form').submit(function(event) {
 		event.preventDefault();
+		// get user submitted text
 		var userText = $(this).find('#user-text').val();
 		reportOnText(removeReturns(userText));
 	});
