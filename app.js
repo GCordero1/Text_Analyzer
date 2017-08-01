3213c7ce26f14454cbeacb8203058cbb48ee6e7d
GCordero1 <Gerson.Cordero@gmail.ccom>
2017-07-31 20:56:13 -0400

    initial commit

--- app.js
+++ app.js
@@ -14,3 +14,25 @@ function countDistinctWords(letters)
 	return distinctWords.letters;
 }
 
+function standardizeText(text)
+	return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+function removeReturns(text) {
+  return text.replace(/\r?\n|\r/g, "");
+
+function reportOnText(text) {
+	var letters = standardizeText(text);
+	var numDistinctWords = countDistinctWords(letters);
+	var numTotalWords = letters.length;
+	var averageWordLength = getAverageWordLength(letters);
+
+	var textReport = $('.js-text-report');
+	textReport.find('.js-word-count').text(numTotalWords);
+	textReport.find('.js-unique-word-count').text(numDistinctWords);
+	textReport.find('.js-average-word-length').text(
+		averageWordLength + " characters");
+	textReport.removeClass('hidden');
+}
+
+
